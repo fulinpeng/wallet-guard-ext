6dec1f7bb12ff80e1c71e63a644cab7d7f796191
fulinpeng <fulinpeng@163.com>
2026-06-16 18:37:11 +0800

    feat: modify vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import manifest from './src/manifest.json'
 
 export default defineConfig({
   plugins: [crx({ manifest })],
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+    strictPort: true,
+  },
 })
